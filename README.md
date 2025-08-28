a73e8f80ab834adc5444a96c42eee5066208dacc
seaeast2 <119303558+notmuchhayden@users.noreply.github.com>
2025-08-28 23:57:04 +0900

    Add project files.

---
+++ README.md
@@ -0,0 +1,1 @@
+# StockDownloader1
---
+++ StockDownloader1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace StockDownloader1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
