ff2e0cf3f7741706993d652e4f293b51f9598a51
seaeast2 <119303558+notmuchhayden@users.noreply.github.com>
2025-08-29 00:29:43 +0900

    MVVM 패턴을 위하여 View 를 분리

---
+++ StockDownloader1/Views/StockListView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace StockDownloader1.Views
+{
+    public partial class StockListView : UserControl
+    {
+        public StockListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
